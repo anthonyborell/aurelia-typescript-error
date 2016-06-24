6fb2eb3144aad46bbd6bf23128092f216d949bf0
Anthony Borell <anthony.borell@immersaview.com>
2016-06-24 15:34:25 +1000

    Changed app.ts to include aurelia-fetch-client

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import {HttpClient} from 'aurelia-fetch-client';
+
 export class App {
   message = 'Hello World!';
+  private m_getHTTP: HttpClient;
+
+  constructor(private http: HttpClient) {
+    this.m_getHTTP = http;
+    this.m_getHTTP.fetch('https://www.google.com.au/')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Google has loaded!');
+      });
+  }
 }
